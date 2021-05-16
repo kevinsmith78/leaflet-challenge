4148c95fc3ed546388df8e10711f263e7819714b
Kevin Smith <kevin.ar.smith@gmail.com>
2021-05-16 14:42:43 -0500

    starting

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,16 +1,18 @@
-// Create the tile layer that will be the background of our map   
-var streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY
+//Create Map Object
+var myMap = L.map("map", {
+    center: [40.7128, -74.0059],
+    zoom: 11
   });
-// Create the Map object with options
-var Mapping = L.map("map-id", {
-    center: [40.73, -74.0059],
-    zoom: 6,
-});
 
-streets.addTo(Mapping);
+//Adding the Tile Layer 
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+}).addTo(myMap);
+
 
  
