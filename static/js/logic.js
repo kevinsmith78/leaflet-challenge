1aa8942cdb2af225538dbbb84ce7e2b2f9baacd6
Kevin Smith <kevin.ar.smith@gmail.com>
2021-05-17 21:33:08 -0500

    adding

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,12 +2,13 @@
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
 
 //Grabbing the GeoJSON data
-d3.json(link).then(function(data) {
+d3.json(link).then(function (data) {
     // Creating a GeoJSON layer with the retrieved data
-    L.geoJson(data).addTo(myMap);
-}); 
+    createFeatures(data.features);
+});
 
 function createFeatures(earthquakeData) {
+    console.log("createFeatures");
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature, layer) {
@@ -17,24 +18,25 @@ function createFeatures(earthquakeData) {
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the onEachFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
-        onEachFeature: onEachFeature, //add popups
-        pointToLayer: pointFunction // add circles
+        onEachFeature: onEachFeature //add popups
+    //    pointToLayer: pointFunction // add circles
     });
 
     // Sending our earthquakes layer to the createMap function
     createMap(earthquakes);
 }
-function createMap(eathquakes) {
-// Add the dark layer
+function createMap(earthquakes) {
+    console.log("createMap");
+    // Add the dark layer
     var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
         id: "dark-v10",
         accessToken: API_KEY
     });
-//Adding the Tile Layer 
+    //Adding the Tile Layer 
     var streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
         maxZoom: 18,
         zoomOffset: -1,
@@ -42,21 +44,21 @@ function createMap(eathquakes) {
         accessToken: API_KEY
     });
 
-// Only one base Layer can shown at a time
+    // Only one base Layer can shown at a time
     var baseMaps = {
         streets: streets,
         darkmap: darkmap
     };
 
- // Create overlay object to hold our overlay layer
+    // Create overlay object to hold our overlay layer
     var overlayMaps = {
         Earthquakes: earthquakes
     };
-//
+    //
 
-    var myMap = L.map("map", {
+    var myMap = L.map("mapid", {
         center: [40.7128, -74.0059],
-        zoom: 5
+        zoom: 5,
         layers: [streets, earthquakes]
     });
     // Add the layer control to the map
@@ -64,37 +66,37 @@ function createMap(eathquakes) {
         collapsed: false
     }).addTo(myMap);
 
-
-//get colour radius from the query URL
-defaultStatus.jason(link, function(data){
-    function style(feature) {
-    return {
-        opacity: 1,
-        fillopacity: 1,
-        fillcolor: getColor(feature.properties.mag),
-        color: "grey",
-        radius: getRadius(feature.properties.mag),
-        stroke: true,
-        weight: 0.5
-    };
-}
-
-function mapColour(mag)
-//Create the variable for the circle in the eathquake map
-function CirColour(size) {
-    if (magnitude >= 5) {
-        return "red";
-    }
-    else if (magnitude >= 4) {
-        return "darkorange";
-    }
-    else if (magnitude >= 3) {
-        return "orange";
-    }
-    else if (magnitude >= 2) {
-        return "yellow";
-    }
-    else if magnitude >= 1 {
-        return "green";
-    }
+    console.log("control.layers");
+    // //get colour radius from the query URL
+    // //defaultStatus.json(link, function(data){
+    //     function style(feature) {
+    //     return {
+    //         opacity: 1,
+    //         fillopacity: 1,
+    //         fillcolor: getColor(feature.properties.mag),
+    //         color: "grey",
+    //         radius: getRadius(feature.properties.mag),
+    //         stroke: true,
+    //         weight: 0.5
+    //     };
 };
+
+// function mapColour(mag) { };
+// //Create the variable for the circle in the eathquake map
+// function CirColour(size) {
+//     if (magnitude >= 5) {
+//         return "red";
+//     }
+//     else if (magnitude >= 4) {
+//         return "darkorange";
+//     }
+//     else if (magnitude >= 3) {
+//         return "orange";
+//     }
+//     else if (magnitude >= 2) {
+//         return "yellow";
+//     }
+//     else if magnitude >= 1 {
+//         return "green";
+//     }
+// };
